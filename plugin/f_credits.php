d97c2c9b30de05b352937963a8fe5dc53f47eef2
RookieExplorer <127442821+RookieExplorer@users.noreply.github.com>
2023-06-11 05:23:30 -0300

    Create f_credits.php

---
+++ plugin/f_credits.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * YELLOW
+ */
+define('YELLOW_CREDIT_PUBLIC', "<b>10</b> Créditos - <b>R$ 15</b>\n<b>50</b> Créditos - <b>R$ 75</b>\n<b>100</b> Créditos - <b>R$ 150</b>");
+define('YELLOW_CREDIT_PRIVATE', "<b>10</b> Créditos - <b>R$ 15</b>\n<b>50</b> Créditos - <b>R$ 75</b>\n<b>100</b> Créditos - <b>R$ 150</b>");
+
+/**
+ * ORANGE
+ */
+define('ORANGE_CREDIT_PUBLIC', "<b>10</b> Créditos - <b>R$ 15</b>\n<b>50</b> Créditos - <b>R$ 75</b>\n<b>100</b> Créditos - <b>R$ 150</b>");
+define('ORANGE_CREDIT_PRIVATE', "<b>10</b> Créditos - <b>R$ 15</b>\n<b>50</b> Créditos - <b>R$ 75</b>\n<b>100</b> Créditos - <b>R$ 150</b>");
